fcf7edb1e28ea60103132a09b63b9796f4fc7537
Daniel Allen <DanielJohn.Allen@dcmail.ca>
2025-10-16 13:59:46 -0400

    all passing tests

--- tests/mymodule/test_student_number.py
+++ tests/mymodule/test_student_number.py
@@ -2,8 +2,8 @@ import pytest
 
 from myapp.mymodule.funcs import *
 # Daniel Allen 
-# Student Number: 100624358
+
 
 @pytest.mark.difficult_operation
 def test_multiply():
-    assert multiply(1, 20) == 58
+    assert multiply(2, 29) == 58
